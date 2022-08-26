e46227f0fd2b1adc27119e40ad1264298f2e34eb
Eli Veilleux <elibchv@gmail.com>
2022-08-25 19:47:47 -0700

    Basic endpoint complete

--- index.js
+++ index.js
@@ -5,18 +5,20 @@ const cors = require('cors');
 const bodyparser = require('body-parser');
 const db = require('./service/connections/db.js');
 
-
-
 app.get('/', (req, res) => {
   res.send('Hello world!');
 })
 
 app.get('/statistics', (req, res) => {
-  db.select('*')
+  db.select('todo_id', 'start_date', 'end_date', 'todo_body', 'category')
     .from('todos')
-    .limit(10)
-    .then((data) => {console.log(data);})
+    .where('user_id', 9)
+    .then((data) => {
+      console.log(data);
+      res.send(data);
+    })
 })
+
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
